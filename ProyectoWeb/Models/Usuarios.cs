72653876df7ad5c96030758e7063622f22b52ff1
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-30 09:48:23 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoWeb/Models/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//para hacer la conexion con la base de datos
+using System.Data.Entity;
+
+namespace ProyectoWeb.Models
+{
+    public class Usuarios
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Id_Proveedor { get; set; }
+        public int No_sirac { get; set;  }
+    }
+
+    public class UsuariosDBContext : DbContext
+    {
+        public DbSet<Usuarios> Users { get; set; }
+    }
+}
